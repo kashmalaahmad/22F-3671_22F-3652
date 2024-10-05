e71f440f73281380c8896784b95521fe0d688f36
Aymanch2764 <f223652@cfd.nu.edu.pk>
2024-10-05 16:50:38 +0500

    Added removeBook function to library class

--- src/Library.java
+++ src/Library.java
@@ -3,14 +3,31 @@ import java.util.List;
 
 public class Library {
 
-	private List<String> books = new ArrayList<>();
+    private List<String> books = new ArrayList<>();
 
-	public void addBook(String bookName) {
-	books.add(bookName);
-	System.out.println(bookName + " added to library");
-	}
+    public void addBook(String bookName) {
+        books.add(bookName);
+        System.out.println(bookName + " added to library");
+    }
 
-	public void listBooks() {
-	System.out.println("Books in library" + books);
-	}
+    public void listBooks() {
+        System.out.println("Books in library: " + books);
+    }
+
+    public void removeBook(String bookName) {
+    	if (books.remove(bookName)) {
+    	System.out.println(bookName + " removed from library.");
+    	} else {
+    	System.out.println(bookName + " not found in library");
+    	}
+    	}
+    public static void main(String[] args) {
+        Library myLibrary = new Library();
+        myLibrary.addBook("Harry Potter");
+        myLibrary.addBook("Lord of the Rings");
+        myLibrary.listBooks();
+    }
+    
+    
+    
 }
